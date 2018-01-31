da9e2d881edc73b3c34bed80c97e23cd8227ea95
npkevin <k.nguyenphams@gmail.com>
2018-01-31 16:43:50 -0500

    added working dynamic dropdown menus

--- a1/javascriptstuff.js
+++ a1/javascriptstuff.js
@@ -2,6 +2,34 @@ $(document).ready(function() {
 	$("#artist").change(function(){
 		//This should change artwork selects to artist's artwork
 		var artistVal = $("#artist").val();
-		alert(artistVal);
+		updateList(artistVal);
 	});
 });
+
+function updateList(av){
+	//clear art work list
+	$("#artwork").children('option:not(:first)').remove();
+
+	//add accordingly
+	for(index in artworks[av]){
+		$("#artwork").append('<option>' + artworks[av][index] + '</option>');
+	}
+
+	//on change go back to disabled option (title of box)
+	$("#artwork").val(0);
+};
+
+
+
+
+
+//<option value="1">Vincent can Gogh</option>
+//<option value="2">Pablo Picasso</option>
+//<option value="3">Claude Money</option>
+//<option value="4">Leonardo da Vinci</option>
+//<option value="5">Michelangelo</option>
+var artworks = [[],
+	["The Starry Night","Irises","The Potato Eaters", "Cafe Terrace at Night"],
+	[],
+	[],
+	[]];
