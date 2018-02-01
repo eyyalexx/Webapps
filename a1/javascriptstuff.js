d32956530f0aab3dfbb7a1c33f633243c5a8bf4c
npkevin <k.nguyenphams@gmail.com>
2018-01-31 22:11:52 -0500

    Submit button disabled unti items selected

--- a1/javascriptstuff.js
+++ a1/javascriptstuff.js
@@ -12,11 +12,21 @@ function updateList(av){
 
 	//add accordingly
 	for(index in artworks[av]){
-		$("#artwork").append('<option>' + artworks[av][index] + '</option>');
+		var artVal = parseInt(index) + 1;
+		$("#artwork").append('<option value="' + artVal + '">' + artworks[av][index] + '</option>');
 	}
 
-	//on change go back to disabled option (title of box)
+	//once populated go back to disabled option (title of box)
 	$("#artwork").val(0);
+
+
+	if ( av > 0) {
+		//enable submit
+		$("#sbtn").removeClass("disabled");
+	} else {
+		//disable submit
+		$("#sbtn").addClass("disabled");
+	}
 };
 
 
@@ -25,11 +35,13 @@ function updateList(av){
 
 //<option value="1">Vincent can Gogh</option>
 //<option value="2">Pablo Picasso</option>
-//<option value="3">Claude Money</option>
+//<option value="3">Claude Monet</option>
 //<option value="4">Leonardo da Vinci</option>
 //<option value="5">Michelangelo</option>
-var artworks = [[],
+var artworks = [
+	["NEVER USED"],
 	["The Starry Night","Irises","The Potato Eaters", "Cafe Terrace at Night"],
-	[],
-	[],
-	[]];
+	["Guernica","Les Demoiselles d'Avignon","The Old Guitarist","Chicago Picasso","The Weeping Woman"],
+	["Bain a la Grenouillere","Bathers at La Grenouillere","Impression, Sunrise","Woman with a Parasol","The Magpie"],
+	["Mona Lisa","The Last Supper","Annunciation","St. John the Baptist","Salvator Mundi",]
+	,[]];
