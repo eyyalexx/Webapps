00cb6a79d55a289d02284a4b0e293ee5c4974fa6
tssaini <taranpreet.s16@gmail.com>
2018-02-01 20:16:22 -0500

    art info added

--- a1_2/javascriptstuff.js
+++ a1_2/javascriptstuff.js
@@ -3,18 +3,18 @@ $(document).ready(function() {
 	//When changes made to artists dropdown menu
 	$("#artist").change(function(){
 		var artistVal = $("#artist").val();
-		console.log(artistVal);
-
-		$("#image").html('<img height="40%" width="40%" src="artists/'+artistVal+'.jpg" alt="Mountain View"/>');
+	
+		$("#image").html('<img height="80%" width="80%" src="artists/'+artistVal+'.jpg" alt="Mountain View"/>');
 		$("#info").html("Hello");
 	});
 
 	//When changes made to artwork dropdown menu
 	$("#artwork").change(function(){
 		var artworkVal = $("#artwork").val();
-		console.log(artworkVal);
-		$("#image").html('<img height="60%" width="60%"src="artwork/'+artworkVal+'.jpg" alt="Mountain View"/>');
 		
+		$("#image").html('<img height="80%" width="80%"src="artwork/'+artworkVal+'.jpg" alt="Mountain View"/>');
+		
+		$("#info").html("Hello");
 	});
 
 	
