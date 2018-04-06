1670b4f9c0d3b259f509710fda81e163d2be78e6
Kevin <k.nguyenphams@gmail.com>
2018-04-06 05:48:58 -0400

    asdasdads

--- a2_Iter2/php/modify.php
+++ a2_Iter2/php/modify.php
@@ -73,18 +73,30 @@
 
 if (isset($_POST['submit'])) {
     $Name = $_POST['Name'];
-    /*$Description = $_POST['Description'];
+    $Description = $_POST['Description'];
     $Birth = $_POST['Birth'];
     $Death = $_POST['Death'];
     $Living = $_POST['Living'];
     $Genres = $_POST['Genres'];
-    $Famous = $_POST['Famous'];*/
+    $Famous = $_POST['Famous'];
 
-    //, Description, Birth, Death, Living, Genres, Famous
-    //, $Description, $Birth, $Death, $Living, $Genres, $Famous
 
-    $conn->query("INSERT Artists (Name) VALUES ($Name)");
-    
+
+    if ($Name == '' || $Description == '' || $Birth == '' || $Death == '' || $Living == '' || $Genres == '' || $Famous == '') {
+        $error = 'ERROR: Please fill in all required fields!';
+        renderForm($Name, $Description, $Birth, $Death, $Living, $Genres, $Famous);
+    }
+    else {
+        if ($stmt = $conn->prepare("INSERT Artists (Name, Description, Birth, Death, Living, Genres, Famous) VALUES (?, ?, ?, ?, ?, ?, ?)")) {
+            $stmt->bind_param("sssssss", $Name, $Description, $Birth, $Death, $Living, $Genres, $Famous);
+            $stmt->execute();
+            $stmt->close();
+        }
+        else {
+            echo "ERROR: Could not prepare SQL statement.";
+        }
+    header("Location: maintain.php");
+    }
 
 } else {
     renderForm();
