2493651f05d92e448a3db978d98c6bc6cce5cda2
Kevin <k.nguyenphams@gmail.com>
2018-04-06 05:20:30 -0400

    asdasdasd

--- a2_Iter2/php/modify.php
+++ a2_Iter2/php/modify.php
@@ -67,98 +67,6 @@
     }
 
 
-
-/*
-
-EDIT RECORD
-
-// if the 'id' variable is set in the URL, we know that we need to edit a record
-if (isset($_GET['id']))
-{
-// if the form's submit button is clicked, we need to process the form
-if (isset($_POST['submit']))
-{
-// make sure the 'id' in the URL is valid
-if (is_numeric($_POST['id']))
-{
-// get variables from the URL/form
-$id = $_POST['id'];
-$firstname = htmlentities($_POST['firstname'], ENT_QUOTES);
-$lastname = htmlentities($_POST['lastname'], ENT_QUOTES);
-
-// check that firstname and lastname are both not empty
-if ($firstname == '' || $lastname == '')
-{
-// if they are empty, show an error message and display the form
-$error = 'ERROR: Please fill in all required fields!';
-renderForm($firstname, $lastname, $error, $id);
-}
-else
-{
-// if everything is fine, update the record in the database
-if ($stmt = $conn->prepare("UPDATE players SET firstname = ?, lastname = ?
-WHERE id=?"))
-{
-$stmt->bind_param("ssi", $firstname, $lastname, $id);
-$stmt->execute();
-$stmt->close();
-}
-// show an error message if the query has an error
-else
-{
-echo "ERROR: could not prepare SQL statement.";
-}
-
-// redirect the user once the form is updated
-header("Location: view.php");
-}
-}
-// if the 'id' variable is not valid, show an error message
-else
-{
-echo "Error!";
-}
-}
-// if the form hasn't been submitted yet, get the info from the database and show the form
-else
-{
-// make sure the 'id' value is valid
-if (is_numeric($_GET['id']) && $_GET['id'] > 0)
-{
-// get 'id' from URL
-$id = $_GET['id'];
-
-// get the recod from the database
-if($stmt = $conn->prepare("SELECT * FROM players WHERE id=?"))
-{
-$stmt->bind_param("i", $id);
-$stmt->execute();
-
-$stmt->bind_result($id, $firstname, $lastname);
-$stmt->fetch();
-
-// show the form
-renderForm($firstname, $lastname, NULL, $id);
-
-$stmt->close();
-}
-// show an error if the query has an error
-else
-{
-echo "Error: could not prepare SQL statement";
-}
-}
-// if the 'id' value is not valid, redirect the user back to the view.php page
-else
-{
-header("Location: view.php");
-}
-}
-}
-
-*/
-
-
 //NEW RECORD
 
 //else {
@@ -174,7 +82,7 @@ if (isset($_POST['submit'])) {
 
 
 
-    if ($Name == '' || $Description == '' || $Birth == '' || $Death == '' || $Living == '' || $Genres == '' || $Famous == '') {
+    /*if ($Name == '' || $Description == '' || $Birth == '' || $Death == '' || $Living == '' || $Genres == '' || $Famous == '') {
         $error = 'ERROR: Please fill in all required fields!';
         renderForm($Name, $Description, $Birth, $Death, $Living, $Genres, $Famous);
     }
@@ -187,19 +95,13 @@ if (isset($_POST['submit'])) {
         else {
             echo "ERROR: Could not prepare SQL statement.";
         }
+    header("Location: maintain.php");
+    }*/
 
-// redirec the user
-header("Location: maintain.php");
-}
-
-}
-// if the form hasn't been submitted yet, show the form
-else
-{
-renderForm();
-}
+} /*else {
+    renderForm();
+}*/
 //}
 
-// close the conn connection
 $conn->close();
 ?>
