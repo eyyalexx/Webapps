8144a855dd0dcec640df9feb5067470f0ea2a7b1
unknown <alex.grigorev@hotmail.com>
2018-04-06 02:04:39 -0400

    css href redirect

--- a2_Iter2/pages/headers/header_main.php
+++ a2_Iter2/pages/headers/header_main.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://jqueryui.com/resources/demos/style.css">
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="../../css/style.css">
     <script src="js/drop_down.js"></script>
     <script src="js/artist_info.js"></script>
     <script src="js/art_info.js"></script>
